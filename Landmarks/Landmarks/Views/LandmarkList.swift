255013d25687ab387470bdb1220dfa6279b79a1b
김성훈 <42074365+SeongHunTed@users.noreply.github.com>
2024-08-24 16:15:08 +0900

    feat: NavigationSplit Added

--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -8,11 +8,20 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    var body: some View {
+	var body: some View {
+		NavigationSplitView {
 			List(landmarks) { landmark in
-				LandmarkRow(landmark: landmark)
+				NavigationLink {
+					LandmarkDetail()
+				} label: {
+					LandmarkRow(landmark: landmark)
+				}
 			}
-    }
+			.navigationTitle("Landmarks")
+		} detail: {
+			Text("Select a Landmark")
+		}
+	}
 }
 
 #Preview {
