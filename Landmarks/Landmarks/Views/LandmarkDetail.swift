f6ee0da43a5436fbc91af6c1240fa6edabb037c4
tedios <tedios@tediosui-noteubug.local>
2024-08-26 12:32:06 +0900

    feat: Favorite Button Added

--- Landmarks/Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -8,43 +8,59 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
-	var landmark: Landmark
-	
-	var body: some View {
-		VStack {
-			MapView(coordinate: landmark.locationCoordinate)
-				.frame(height: 300)
-			
-			CircleImage(image: landmark.image)
-				.offset(y: -130)
-				.padding(.bottom, -130)
-			
-			VStack(alignment: .leading) {
-				Text(landmark.name)
-					.font(.title)
-					.foregroundStyle(.black)
-				
-				HStack {
-					Text(landmark.park)
-						.font(.subheadline)
-					Spacer()
-					Text(landmark.state)
-						.font(.subheadline)
-				}
-				
-				Divider()
-				
-				Text("About \(landmark.name)")
-					.font(.title2)
-				Text(landmark.description)
-			}
-			.padding()
-		}
-		.navigationTitle(landmark.name)
-		.navigationBarTitleDisplayMode(.inline)
-	}
+  @Environment(ModelData.self) var modelData
+  var landmark: Landmark
+  
+  var landmarkIndex: Int {
+    modelData.landmarks.firstIndex(where: { $0.id == landmark.id})!
+  }
+  
+  var body: some View {
+    @Bindable var modelData = modelData
+    
+    ScrollView {
+      MapView(coordinate: landmark.locationCoordinate)
+        .frame(height: 300)
+      
+      
+      CircleImage(image: landmark.image)
+        .offset(y: -130)
+        .padding(.bottom, -130)
+      
+      
+      VStack(alignment: .leading) {
+        HStack {
+          Text(landmark.name)
+            .font(.title)
+          FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+        }
+        
+        
+        HStack {
+          Text(landmark.park)
+          Spacer()
+          Text(landmark.state)
+        }
+        .font(.subheadline)
+        .foregroundStyle(.secondary)
+        
+        
+        Divider()
+        
+        
+        Text("About \(landmark.name)")
+          .font(.title2)
+        Text(landmark.description)
+      }
+      .padding()
+    }
+    .navigationTitle(landmark.name)
+    .navigationBarTitleDisplayMode(.inline)
+  }
 }
 
 #Preview {
-    LandmarkDetail(landmark: ModelData().landmarks[0])
+  let modelData = ModelData()
+  return LandmarkDetail(landmark: ModelData().landmarks[0])
+    .environment(modelData)
 }
