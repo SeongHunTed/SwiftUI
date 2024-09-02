924736299c9e37f41ef8abe3aec8a81770835baa
SeongHunTed <4047ksh@naver.com>
2024-09-02 12:16:10 +0900

    feat: Animation Adapted

--- Landmarks/Landmarks/Model/Hike.swift
+++ Landmarks/Landmarks/Model/Hike.swift
@@ -12,7 +12,7 @@ struct Hike: Codable, Hashable, Identifiable {
   var name: String
   var distance: Double
   var difficulty: Int
-  var observation: [Observation]
+  var observations: [Observation]
   
   static var formatter = LengthFormatter()
   
