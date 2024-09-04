3ca502ce1002b75d1af9135faa7255912f110a42
김성훈 <42074365+SeongHunTed@users.noreply.github.com>
2024-09-04 21:39:57 +0900

    feat: Category View

--- Landmarks/Landmarks/Views/ContentView.swift
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -8,12 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+	@State private var selection: Tab = .featured
+	
+	enum Tab {
+		case featured
+		case list
+	}
+	
+	var body: some View {
+		TabView(selection: $selection) {
+			CategoryHome()
+				.tabItem {
+					Label("Featured", systemImage: "star")
+				}
+				.tag(Tab.featured)
+			
+			
 			LandmarkList()
-    }
+				.tabItem {
+					Label("List", systemImage: "list.bullet")
+				}
+				.tag(Tab.list)
+		}
+	}
 }
 
 #Preview {
-    ContentView()
-        .environment(ModelData())
+	ContentView()
+		.environment(ModelData())
 }
