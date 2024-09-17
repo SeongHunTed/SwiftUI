9256e462a89bd10c17594c4bc43711317c821e75
김성훈 <42074365+SeongHunTed@users.noreply.github.com>
2024-09-17 19:42:36 +0900

    redo: Lesson1-1

---
+++ Landmark2/Landmark2/Landmark2App.swift
@@ -0,0 +1,17 @@
+//
+//  Landmark2App.swift
+//  Landmark2
+//
+//  Created by Hoon on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Landmark2App: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
---
+++ Landmark_Again/Landmark_Again/Landmark_AgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmark_AgainApp.swift
+//  Landmark_Again
+//
+//  Created by Hoon on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Landmark_AgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
